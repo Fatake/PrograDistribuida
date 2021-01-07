5ed0992baf80843c94f03bbb5520f5e484fcbe52
Fatake <pace_lozano@hotmail.com>
2021-01-07 11:30:52 -0600

    identacion cambiada

--- Practica 1/Sistema.java
+++ Practica 1/Sistema.java
@@ -9,39 +9,39 @@
  * Class Sistema
  */
 public class Sistema {
-  //
-  // Constructors
-  //
-  public Sistema () { };
+    //
+    // Constructors
+    //
+    public Sistema () { };
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-      // Contenedor de piezas
-      Contenedor contenedor = new Contenedor(0, 50);
+        // Contenedor de piezas
+        Contenedor contenedor = new Contenedor(0, 50);
 
-      // Brazos
-      Brazo brazo1 = new Brazo(1, 12, contenedor);
-      Brazo brazo2 = new Brazo(2, 7, contenedor);
+        // Brazos
+        Brazo brazo1 = new Brazo(1, 12, contenedor);
+        Brazo brazo2 = new Brazo(2, 7, contenedor);
 
-      // Arreglo de hilos
-      Thread[] hilos = new Thread[2];
+        // Arreglo de hilos
+        Thread[] hilos = new Thread[2];
 
-      hilos[0] = new Thread(brazo1);
-      hilos[0].start();
+        hilos[0] = new Thread(brazo1);
+        hilos[0].start();
 
-      hilos[1] = new Thread(brazo2);
-      hilos[1].start();
-      
-      // En espera a terminar los procesos
-      for (int i = 0; i < hilos.length; i++) {
-          try{
-              hilos[i].join();
-          } catch (InterruptedException e) {
-              e.printStackTrace();
-          }
-      }
-      System.out.println("[*] End Program");
-  }
+        hilos[1] = new Thread(brazo2);
+        hilos[1].start();
+        
+        // En espera a terminar los procesos
+        for (int i = 0; i < hilos.length; i++) {
+            try{
+                hilos[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("[*] End Program");
+    }
 }
