9fba96a62ec7d1bdde0254d9e113d85e48fe0b75
Fatake <pace_lozano@hotmail.com>
2021-02-25 12:22:26 -0600

    Poniendo comentarios

--- Practica3UDP/UDPClient.java
+++ Practica3UDP/UDPClient.java
@@ -1,24 +1,56 @@
 import java.net.*;
 import java.io.*;
+
+/**
+ * Cliente
+ */
 public class UDPClient{
     public static void main(String args[]){
 		// args give message contents and destination hostname
+
+		// Crea un Socket Datagrama
 		DatagramSocket aSocket = null;
 		try {
+			// Crea el soquet
 			aSocket = new DatagramSocket();
-			//String c= "Hola distribuido";
-			byte [] m = args[0].getBytes();
+
+			// Genera un ID del servidor
 			InetAddress aHost = InetAddress.getByName(args[1]);
+
+			// Puerto del servidor
 			int serverPort = 6789;
-			DatagramPacket request =
-			 	new DatagramPacket(m, args[0].length(), aHost, serverPort);
-			aSocket.send(request);
+
+			
+			// Recibe mensaje por agumentos
+			byte [] mensaje = args[0].getBytes(); 
+
+			// Crea un datagrama de salida
+			DatagramPacket salida = new DatagramPacket(mensaje, 
+													args[0].length(), 
+													aHost, 
+													serverPort);
+
+			// Envia el datagrama
+			aSocket.send(salida);
+
+			// Crea un buffer de entrada
 			byte[] buffer = new byte[1000];
-			DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-			aSocket.receive(reply);
-			System.out.println("Reply: " + new String(reply.getData()));
-		}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
-		}catch (IOException e){System.out.println("IO: " + e.getMessage());
-		}finally {if(aSocket != null) aSocket.close();}
+
+
+			// Datagrama de entrada
+			DatagramPacket entrada = new DatagramPacket(buffer, buffer.length);
+			aSocket.receive(entrada);
+
+			System.out.println("Respuesta: " + new String(entrada.getData()));
+
+		}catch (SocketException e){
+			System.out.println("Socket: " + e.getMessage());
+		}catch (IOException e){
+			System.out.println("IO: " + e.getMessage());
+		}finally {
+			if(aSocket != null){
+				aSocket.close();
+			} 
+		}
 	}
 }
--- Practica3UDP/UDPServer.java
+++ Practica3UDP/UDPServer.java
@@ -1,21 +1,50 @@
 import java.net.*;
 import java.io.*;
+
+/**
+ * 
+ */
 public class UDPServer{
     public static void main(String args[]){ 
     	DatagramSocket aSocket = null;
 		try{
 	    	aSocket = new DatagramSocket(6789);
-					// create socket at agreed port
+			
+			// create socket at agreed port
 			byte[] buffer = new byte[1000];
- 			while(true){
- 				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
-  				aSocket.receive(request);     
-    			DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), 
-    				request.getAddress(), request.getPort());
-    			aSocket.send(reply);
-    		}
-		}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
-		}catch (IOException e) {System.out.println("IO: " + e.getMessage());
-		}finally {if(aSocket != null) aSocket.close();}
+
+			// Sigue escuchando peticiones
+			while(true){
+				// buffer <- Es lo que se va a leer o escribir
+				// lenght es cuando se va a leer o escribir
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+
+				// Recibe datos
+				aSocket.receive(request);
+				String mensaje = new String(request.getData());
+				System.out.println("[i] Recibiendo: "+mensaje);
+
+				// Envia datos a un nueva datagrama 
+				// donde DatagramPacket(buffer,lenght, addres, port)
+				// buffer y lenght es lo que se va a enviar y el tamaño
+				// address del destino
+				// port puerto del destino
+				DatagramPacket reply = new DatagramPacket(request.getData(),
+														request.getLength(), 
+														request.getAddress(), 
+														request.getPort());
+				
+				// envia el datagrama
+				aSocket.send(reply);
+			}
+		}catch (SocketException e){
+			System.out.println("Socket: " + e.getMessage());
+		}catch (IOException e) {
+			System.out.println("IO: " + e.getMessage());
+		}finally {
+			if(aSocket != null){
+				aSocket.close();
+			}	
+		}
     }
 }
