5796ac0476b3436a9029769de73df1f696c8e39f
Fatake <pace_lozano@hotmail.com>
2021-03-05 21:06:46 -0600

    Creando subcarpeta parte 1

---
+++ Practica4/Parate1/HelloServer.java
@@ -0,0 +1,17 @@
+import java.rmi.Naming;
+
+public class HelloServer {
+  /**
+   * Server program for the "Hello, world!" example.
+   * @param argv The command line arguments which are ignored.
+   */
+  public static void main (String[] argv) {
+    System.out.println("[i] Iniciando Servidor RMI");
+    try {
+      Naming.rebind ("rmi://localhost:1099/Hola", new Hello ("Prueba de clientes remotos"));
+      System.out.println ("[i] Registro con Naming Completo.");
+    } catch (Exception e) {
+      System.out.println ("[i] Fallo en el registro " + e);
+    }
+  }
+}
