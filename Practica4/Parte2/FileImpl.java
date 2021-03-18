fde7657ceadd1b167283e70fa2837d084e501a44
IYRH <inesyaelinrojasherta.31@gmail.com>
2021-03-17 20:31:12 -0600

    Metodo escribe

--- Practica4/Parte2/FileImpl.java
+++ Practica4/Parte2/FileImpl.java
@@ -131,7 +131,25 @@ public class FileImpl extends UnicastRemoteObject
 
    @Override
    public void escribe(OutputStream os) throws RemoteException {
-      // TODO Auto-generated method stub
+      File origen = new File(nombre);
+		File destino = new File("destino.txt");
+		try {
+		  InputStream in = new FileInputStream(origen);
+		  os = new FileOutputStream(destino);
+						
+		  byte[] buf = new byte[1024];
+		  int len;
+
+		  while ((len = in.read(buf)) > 0) {
+		    os.write(buf, 0, len);
+		}
+
+		in.close();
+		os.close();
+      
+		} catch (IOException ioe){
+		  ioe.printStackTrace();
+		}
       
    }
 
