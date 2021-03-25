a0ec5fac70e6d20e91f158cd6c96364aeecf5b1c
Fatake <pace_lozano@hotmail.com>
2021-03-25 12:29:50 -0600

    Problemas en la política

--- Practica4/Parte2/FileServer.java
+++ Practica4/Parte2/FileServer.java
@@ -4,17 +4,22 @@ import java.rmi.RMISecurityManager;
 
 public class FileServer {
    public static void main(String argv[]) {
-        System.out.println("Iniciando....");
+        System.out.println("\n<------Server On------->");
         if(System.getSecurityManager() == null) {
             System.setSecurityManager(new RMISecurityManager());
         }
+
         try {
             FileInterface fi = new FileImpl("hola.txt");
+            System.out.println("[i] Objeto remoto creado");
+
             Naming.rebind("//127.0.0.1/FileServer", fi);
-            System.out.println("Cargando el archivo en sistema....");
+            System.out.println("[i] Objeto remoto subido");
         } catch(Exception e) {
-            System.out.println("FileServer: "+e.getMessage());
+            System.out.println("[!]FileServer: "+e.getMessage());
+            System.out.println("\n");
             e.printStackTrace();
+            System.exit(1);
         }
    }
 }
--- Practica4/Parte2/win.sh
+++ Practica4/Parte2/win.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 #start rmiregistry 9878
 rm -rf *.class
 javac *.java
 cp *.class ..
 cp cli.sh ..
-echo "Compilado Correcto"
-java -Djava.security.policy=politica.txt FileServer
+echo "[i] Compilado Correcto"
+
+java -Djava.security.policy=politica.txt -Djava.rmi.server.hostname=127.0.0.1 FileServer
