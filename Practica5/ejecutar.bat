da70d01cc76785fb1ea13ba3994506b438b6828a
IYRH <inesyaelinrojasherta.31@gmail.com>
2021-05-20 14:45:45 -0500

    Terminando de ejecutar ejemplo, agregando bat

---
+++ Practica5/ejecutar.bat
@@ -0,0 +1,33 @@
+@echo off
+echo "[!] Borrando .class y FileInterfacePackage"
+del *.class 
+rmdir /S /Q FileInterfacePackage
+
+echo "[i] Compilando idl servidor"
+start C:\jdk1.3.1_28\bin\idlj.exe -fserver FileInterface.idl
+ping 192.0.2.1 -n 1 -w 250 >nul 
+
+echo "[i] Colmpilando idl cliente"
+start C:\jdk1.3.1_28\bin\idlj.exe -fclient FileInterface.idl
+ping 192.0.2.1 -n 1 -w 250 >nul  
+
+echo "[i] Compilando servidor"
+start C:\jdk1.3.1_28\bin\javac.exe FileServer.java
+ping 192.0.2.1 -n 1 -w 250 >nul 
+
+echo "[i] Compilando cliente"
+start C:\jdk1.3.1_28\bin\javac.exe FileClient.java
+ping 192.0.2.1 -n 1 -w 250 >nul 
+
+echo "[+] Lanzando tname server"
+start C:\jdk1.3.1_28\bin\tnameserv.exe -ORBinitialPort 2500
+ping 192.0.2.1 -n 1 -w 250 >nul 
+
+echo "[+] Lanzando servidor"
+start C:\jdk1.3.1_28\bin\java.exe FileServer
+ping 192.0.2.1 -n 1 -w 250 >nul 
+
+echo "[+] Lanzando cliente"
+start C:\jdk1.3.1_28\bin\java.exe FileClient hello.txt -ORBinitialPort 2500
+ping 192.0.2.1 -n 1 -w 250 >nul 
+pause
