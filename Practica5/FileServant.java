dcc99c2d90827fcc338b46ffcd162934c3d82d0d
Fatake <pace_lozano@hotmail.com>
2021-05-20 19:02:58 -0500

    agregando metodos

--- Practica5/FileServant.java
+++ Practica5/FileServant.java
@@ -1,6 +1,12 @@
-import java.io.*; 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List; 
 
 public class FileServant extends _FileInterfaceImplBase {
+
    public byte[] downloadFile(String fileName){
       File file = new File(fileName);
       byte buffer[] = new byte[(int)file.length()];
@@ -14,5 +20,193 @@ public class FileServant extends _FileInterfaceImplBase {
          e.printStackTrace();
       }
       return(buffer); 
+   }
+
+   public File[] listar(){  
+      File[] archivos  = null;
+      try {
+         File dir = new File(".");
+         if(dir.exists()) {
+            if(dir.isDirectory()) {
+               archivos = dir.listFiles();
+            }
+         }
+      } catch(Exception e){
+         System.out.println("Error: "+e.getMessage());
+         e.printStackTrace();
+      }
+      return archivos;
+   }
+
+   public int cuentaLineas(String nombreArchivo){ 
+      BufferedReader reader;
+      int contador = 0;
+		try {
+			reader = new BufferedReader(new FileReader(nombreArchivo));
+			String line = reader.readLine();
+         
+			while (line != null) {
+				line = reader.readLine();
+            contador ++;
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+      return contador;
+   }
+   
+   public int cuentaVocales(String nombreArchivo){
+      BufferedReader reader = null;
+      int contador = 0;
+		try {
+			reader = new BufferedReader(new FileReader(nombreArchivo));
+   
+			String line = "";
+         
+			do {
+				line = reader.readLine();
+            if(line == null){
+               break;
+            }
+            System.out.println(contador);
+            System.out.print(line);
+            int res = 0;
+            String fraseMin = line.toLowerCase();
+            for (int i = 0; i < fraseMin.length(); ++i) {
+               char aux = fraseMin.charAt(i);
+               if("aeiou".contains(String.valueOf(aux).toLowerCase())){
+                  res ++;
+               }
+            }
+            contador += res;
+			}while (line != null);
+         return contador;	
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+      finally{
+         try {
+            reader.close();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      } 
+      return contador;
+   }
+
+   public Boolean escribe(String destino, String mensaje){
+		File fDestino = new File(destino);
+		try {
+		   OutputStream os = new FileOutputStream(fDestino,true);
+         os.write("\n".getBytes(),0,"\n".length());
+		   os.write(mensaje.getBytes(),0,mensaje.length());
+		   os.close();
+		} catch (IOException ioe){
+		   ioe.printStackTrace();
+         return false;
+		}
+      return true;
+   }
+
+   public String[] imprimir(String archivo){
+      BufferedReader reader;
+      File Farchivo = new File(archivo);
+      List <String> temporal = new  ArrayList <String>();
+      String[] lineas = null;
+		try {
+			reader = new BufferedReader(new FileReader(Farchivo));
+			String line = null;
+         
+			do{
+				line = reader.readLine();
+            if (line == null){
+               break;
+            }
+            temporal.add(line);
+			}while (line != null);
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+      lineas = new String[temporal.size()];
+      lineas = temporal.toArray(lineas);
+      return lineas;
+   }
+
+   public void respaldar(String nombreArchivo){
+      //utilizar fileread
+      InputStream inputStream = null;
+      OutputStream outputStream = null;
+      try {
+         // Nombre del archivo respaldo
+         File archivoRespaldo = new File(nombreArchivo+"-respaldo");
+         File archivoOriginal = new File(nombreArchivo);
+         // no existe
+         if (!archivoRespaldo.exists()) {
+            archivoRespaldo.createNewFile();
+         }
+
+         inputStream = new FileInputStream(archivoOriginal);
+         outputStream = new FileOutputStream(archivoRespaldo);
+         byte[] buffer = new byte[(int)archivoOriginal.length()];
+
+         int length;
+         while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+         }
+         inputStream.close();
+         outputStream.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void copiar(String nombreOrigen,String nombreArchivoDestino){
+      File origen = new File(nombreOrigen);
+		File destino = new File(nombreArchivoDestino);
+		try {
+         FileReader fr = new FileReader(origen); 
+         FileWriter salida = new FileWriter(destino, true);
+
+         BufferedReader br = new BufferedReader(fr);
+
+         String line;
+         salida.append("\n");
+         while((line = br.readLine())!=null)  {  
+            salida.append(line);
+            salida.append("\n");
+         }  
+               
+         br.close();
+         salida.close();
+		} catch (IOException ioe){
+		   ioe.printStackTrace();
+		}
+   }
+   
+   public void renombrar(String nombreArchivo,String nombreNuevo){
+      File f = new File(nombreArchivo);
+      Path source = Paths.get(f.getAbsolutePath());
+
+      try{
+         Files.move(source, source.resolveSibling(nombreNuevo));
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   public void eliminar(String nombreArchivo){
+      try{
+         File archivo = new File(nombreArchivo);
+         boolean estatus = archivo.delete();
+         if (!estatus) {
+            System.out.println("[!] Error no se ha podido eliminar el  archivo");
+         }else{
+            System.out.println("[i] Se ha eliminado el archivo exitosamente");
+         }
+      }catch(Exception e){
+         System.out.println(e);
+      }
    }
 }
--- Practica5/FileServer.java
+++ Practica5/FileServer.java
@@ -20,21 +20,22 @@ public class FileServer {
          FileServant fileRef = new FileServant();
          orb.connect(fileRef);
          // get the root naming context
-         org.omg.CORBA.Object objRef =
-            orb.resolve_initial_references("NameService");
+         org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
          NamingContext ncRef = NamingContextHelper.narrow(objRef);
          // Bind the object reference in naming
          NameComponent nc = new NameComponent("FileTransfer", " ");
          NameComponent path[] = {nc};
          ncRef.rebind(path, fileRef);
-         System.out.println("Server started....");
+
+         System.out.println("[i] Servidor inciando\n[+] Escuchando...");
          // Wait for invocations from clients
          java.lang.Object sync = new java.lang.Object();
          synchronized(sync){
+            System.out.println("[+] Nuevo cliente: "+sync.toString());
             sync.wait();
          }
       } catch(Exception e) {
-         System.err.println("ERROR: " + e.getMessage());
+         System.err.println("[!] ERROR: " + e.getMessage());
          e.printStackTrace(System.out);
       }
    }
